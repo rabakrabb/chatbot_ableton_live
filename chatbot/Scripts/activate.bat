67ee2193c07bac3daf4c8721a4a77c9212b25a42
rabakrabb <rabadab@gmail.com>
2025-05-15 12:41:47 +0200

    Lägg till ny app med google-generativeai stöd och uppdateringar

--- chatbot/Scripts/activate.bat
+++ chatbot/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\DS24\ds24_dl\kunskapskontroll_2\chatbot
+set VIRTUAL_ENV=C:\DS24\ds24_dl\chatbot_ableton_live\chatbot
 
 if not defined PROMPT set PROMPT=$P$G
 
